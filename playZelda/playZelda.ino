cb7d77e806b6328553eb3d4129dfefa8e4c4bdc4
khedoros <khedoros@gmail.com>
2023-12-09 21:45:14 -0800

    Add file and directory for Arduino sketch

---
+++ playZelda/playZelda.ino
@@ -0,0 +1,11 @@
+const int timeDivisions = 8'000;  // timeslices per second in my arduino program
+uint16_t noiseWavelengths[] = {4, 8, 16, 32, 64, 96, 128, 160, 202, 254, 380, 508, 762, 1016, 2034, 4068};
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
