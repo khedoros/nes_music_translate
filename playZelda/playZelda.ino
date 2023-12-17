45311b4af8bcf2f5341ce9ce160029c9ce7d71f6
khedoros <khedoros@gmail.com>
2023-12-17 13:58:58 -0800

    Yay, 3 channels working!
    - Added Triangle and Noise channels to event processing
    - Added noise channel to output
    - Triangle needs debugging. It kills everything else when you actually add the samples
    - Fixed wavelength counters for more accurate reproduction of intended notes
    - Fixed noise LFSR (init'd value to 1)
    - Triangle currently using 16 steps instead of 32; just means a lower-res triangle
    - Removed a lot of dead code and unnecessary serial output

--- playZelda/playZelda.ino
+++ playZelda/playZelda.ino
@@ -3,6 +3,7 @@
 const int AUDIOPIN = 7;
 const int tsTimeDivisions = 8000;  // timeslices per second in the timestamps
 const int timeDivisions = 31250; // timeslices per second in the arduino program itself
+const uint8_t apuCyclesPerSample = 29;
 const uint16_t noiseWavelengths[] = { 4, 8, 16, 32, 64, 96, 128, 160, 202, 254, 380, 508, 762, 1016, 2034, 4068 };
 const uint8_t lengths[32] = { 10, 254, 20, 2, 40, 4, 80, 6, 160, 8, 60, 10, 14, 12, 26, 14, 12, 16, 24, 18, 48, 20, 96, 22, 192, 24, 72, 26, 16, 28, 32, 30 };
 
@@ -11,7 +12,7 @@ const uint8_t sq_duty[4][8] = { { 0, 1, 0, 0, 0, 0, 0, 0 },
                                 { 0, 1, 1, 1, 1, 0, 0, 0 },
                                 { 1, 0, 0, 1, 1, 1, 1, 1 } };
 
-const uint8_t tri_duty[32] = { 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+const uint8_t tri_duty[16] = { 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7 };
 
 unsigned long nextTS;
 uint8_t nextReg;
@@ -26,11 +27,11 @@ uint8_t sq2LenCnt = 0;
 uint8_t triLenCnt = 0;
 uint8_t noiseLenCnt = 0;
 
-// Remaining 8KHz timer ticks until we clock the duty cycles
-uint16_t sq1WavelengthCnt = 0;
-uint16_t sq2WavelengthCnt = 0;
-uint16_t triWavelengthCnt = 0;
-uint16_t noiseWavelengthCnt = 0;
+// Remaining NES APU ticks until we clock the duty cycles
+int16_t sq1WavelengthCnt = 0;
+int16_t sq2WavelengthCnt = 0;
+int16_t triWavelengthCnt = 0;
+int16_t noiseWavelengthCnt = 0;
 
 // Wavelength counter reset values
 uint16_t sq1WavelengthReset = 0;
@@ -44,7 +45,7 @@ uint8_t sq2DutyIdx = 0;
 uint8_t triDutyIdx = 0;
 
 const uint8_t squareDuties = 8;
-const uint8_t triDuties = 32;
+const uint8_t triDuties = 16;
 
 // Square channel duty cycle types
 uint8_t sq1DutyCycle = 0;
@@ -56,8 +57,8 @@ uint8_t sq2Vol = 0;
 uint8_t noiseVol = 0;
 
 // LFSR data for noise generation
-uint16_t noiseLFSR = 0;
-bool noiseType;
+uint16_t noiseLFSR = 1;
+uint8_t noiseType;
 uint8_t noiseSample;
 
 // length-clocking step
@@ -67,34 +68,16 @@ volatile uint8_t *audioOut;
 uint8_t setLow;
 uint8_t setHigh;
 
-void clockNoise() {
-  if (noiseType) {
-    noiseSample = bitRead(noiseLFSR, 8) ^ bitRead(noiseLFSR, 14);
-  } else {
-    noiseSample = bitRead(noiseLFSR, 13) ^ bitRead(noiseLFSR, 14);
-  }
-  noiseLFSR <<= 1;
-  noiseLFSR |= noiseSample;
-}
-
 bool nextEvent() {
   unsigned long nextEv = pgm_read_dword_near(music+eventIdx);
-  //Serial.println(nextEv);
   nextTS = (nextEv & 0x000fffff)<<2;
   nextReg = nextEv >> 28;
   nextVal = ((nextEv >> 20) & 0xff);
-  //Serial.println(nextTS);
-  //Serial.println(nextReg);
-  //Serial.println(nextVal);
   eventIdx++;
   return nextTS > 0;
 }
 
 void setup() {
-  cli();
-  //Serial.begin(9600);
-  //while(!Serial);
-  Serial.println("Setup");
   pinMode(AUDIOPIN, OUTPUT);
   pinMode(13, OUTPUT);
   nextEvent();
@@ -117,9 +100,6 @@ void setup() {
 	audioOut = portOutputRegister(audioPinPort);
   setLow = *audioOut & ~audioPinBit;
   setHigh = *audioOut | audioPinBit;
-
-  Serial.println("Setup done");
-  sei();
 }
 
 // Clock the note length counters at 96 and 192Hz
@@ -140,23 +120,17 @@ void loop() {
 
 // Process register writes, generate audio
 ISR(TIMER1_OVF_vect) {
-  //Serial.print("t1: ");
-  //Serial.println(timeStamp);
-  // Reset timer
-  TCNT1 = 65535 - 64;
-  timeStamp++;
   if (nextSample > 0) {
-    //Serial.println(nextSample);
-    *audioOut = setHigh;       // Turn on pin, start timer to turn it off
-
-    TCNT3 = 65535 - (8 * nextSample);  // Leave the pin high for between 8 and 512 cycles, depending on sample
-    TCCR3B = B00000001;                 // Clock timer 3 at 1x of clock speed
+    *audioOut = setHigh;               // Turn on pin, start timer to turn it off
+    TCNT3 = 65535 - (6 * nextSample);  // Leave the pin high for between 8 and 512 cycles, depending on sample
+    TCCR3B = B00000001;                // Start timer 3, clocked at 1x of clock speed
   }
 
+  // Reset timer 1 to 64*8 cycles from now (31250Hz repeat rate)
+  TCNT1 = 65535 - 64;
+  timeStamp++;
+
   if (timeStamp >= nextTS) {
-    // nextTS = nextEvent & 0x000fffff;
-    // nextReg = nextEvent >> 28;
-    // nextVal = ((nextEvent >> 20) & 0xff);
     switch (nextReg) {
       // SQ1
       case 0:
@@ -171,9 +145,6 @@ ISR(TIMER1_OVF_vect) {
         sq1WavelengthReset &= 0x00ff;
         sq1WavelengthReset |= (uint16_t(nextVal & 0x7) << 8);
         sq1LenCnt = lengths[nextVal >> 3];
-        //Serial.println(sq1WavelengthReset);
-        // Serial.println(sq1LenCnt);
-       //Serial.println("");
         break;
       // SQ2
       case 4:
@@ -190,13 +161,29 @@ ISR(TIMER1_OVF_vect) {
         sq2LenCnt = lengths[nextVal >> 3];
         break;
       // TRI
-      case 8: break;
-      case 10: break;
-      case 11: break;
+      case 8: // Not used in Zelda's music
+        //triLenCnt = nextVal & 0x7f;
+        break;
+      case 10:
+        triWavelengthReset &= 0xff00;
+        triWavelengthReset |= nextVal;
+        break;
+      case 11:
+        triWavelengthReset &= 0x00ff;
+        triWavelengthReset |= (uint16_t(nextVal & 0x07) << 8);
+        triLenCnt = lengths[nextVal >> 3];
+        break;
       // NOISE
-      case 12: break;
-      case 14: break;
-      case 15: break;
+      case 12:
+        noiseVol = nextVal & 0xf;
+        break;
+      case 14:
+        noiseType = nextVal>>7;
+        noiseWavelengthReset = noiseWavelengths[nextVal & 0xf];
+        break;
+      case 15:
+        noiseLenCnt = lengths[nextVal >> 3];
+        break;
     }
     if (!nextEvent()) {  // Hit the last event. Let's restart!
       eventIdx = 0;
@@ -207,50 +194,63 @@ ISR(TIMER1_OVF_vect) {
 
   // Generate the samples!
   nextSample = 0;
-  if (sq1LenCnt > 0 && sq1Vol > 0 && sq1WavelengthReset > 0) {
-    //digitalWrite(13, HIGH);
-    if (sq1WavelengthCnt == 0) {
-      sq1WavelengthCnt = sq1WavelengthReset>>5;
+
+  // Generate Square Channel 1
+  if (sq1LenCnt > 0 && sq1Vol > 0 && sq1WavelengthReset > 7) {
+    if (sq1WavelengthCnt <= 0) {
+      // Clock the SQ1's duty cycle
+      sq1WavelengthCnt += sq1WavelengthReset;
       sq1DutyIdx++;
       if (sq1DutyIdx >= squareDuties) sq1DutyIdx = 0;
     }
-    nextSample += ((sq_duty[sq1DutyCycle][sq1DutyIdx] * sq1Vol)<<1);
-    sq1WavelengthCnt--;
-  } else {
-    //digitalWrite(13, LOW);
+    nextSample += ((sq_duty[sq1DutyCycle][sq1DutyIdx] * sq1Vol));
+    sq1WavelengthCnt -= apuCyclesPerSample;
   }
-  if (sq2LenCnt > 0 && sq2Vol > 0 && sq2WavelengthReset > 0) {
-    if (sq2WavelengthCnt == 0) {
-      sq2WavelengthCnt = sq2WavelengthReset>>5;
+
+  // Generate Square Channel 2
+  if (sq2LenCnt > 0 && sq2Vol > 0 && sq2WavelengthReset > 7) {
+    if (sq2WavelengthCnt <= 0) {
+      // Clock the noise SQ2's duty cycle
+      sq2WavelengthCnt += sq2WavelengthReset;
       sq2DutyIdx++;
       if (sq2DutyIdx >= squareDuties) sq2DutyIdx = 0;
     }
-    nextSample += ((sq_duty[sq2DutyCycle][sq2DutyIdx] *sq2Vol)<<1);
-    sq2WavelengthCnt--;
+    nextSample += ((sq_duty[sq2DutyCycle][sq2DutyIdx] *sq2Vol));
+    sq2WavelengthCnt -= apuCyclesPerSample;
   }
+
+  // Generate Triangle
   if (triWavelengthReset > 0 && triLenCnt > 0) {
-    if (triWavelengthCnt == 0) {
-      triWavelengthCnt = triWavelengthReset;
+    if (triWavelengthCnt <= 0) {
+      // Clock the triangle's duty cycle
+      triWavelengthCnt += triWavelengthReset;
       triDutyIdx++;
       if (triDutyIdx >= triDuties) triDutyIdx = 0;
     }
-    nextSample += tri_duty[triDutyIdx];
-    triWavelengthCnt--;
+    //nextSample += tri_duty[triDutyIdx];
+    triWavelengthCnt -= apuCyclesPerSample;
   }
+
+  // Generate Noise
   if (noiseWavelengthReset > 0 && noiseLenCnt > 0 && noiseVol > 0) {
-    if (noiseWavelengthCnt == 0) {
-      clockNoise();
-      noiseWavelengthCnt = noiseWavelengthReset>>8;
+    if (noiseWavelengthCnt <= 0) {
+        // Clock the noise generator's duty cycle
+        if (noiseType) {
+          noiseSample = bitRead(noiseLFSR, 8) ^ bitRead(noiseLFSR, 14);
+        } else {
+        noiseSample = bitRead(noiseLFSR, 13) ^ bitRead(noiseLFSR, 14);
+        }
+        noiseLFSR <<= 1;
+        noiseLFSR |= noiseSample;
+        noiseWavelengthCnt += noiseWavelengthReset;
     }
     nextSample += noiseSample * noiseVol;
-    noiseWavelengthCnt--;
+    noiseWavelengthCnt -= apuCyclesPerSample;
   }
-  //Serial.write(nextSample);
 }
 
+// Switch pin off when intended PWM length is reached
 ISR(TIMER3_OVF_vect) {
-  //Serial.println("t3");
   TCCR3B = B00000000;  // Stop timer3
-  //digitalWrite(AUDIOPIN, LOW);
   *audioOut = setLow;
 }
